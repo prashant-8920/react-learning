3930660623cf5d666b4dc936e9189e5803812b37
prashant varnwal <prashant892005@gmail.com>
2026-02-05 08:10:46 +0530

    fetch users from API and display responsive user cards using React hooks

---
+++ 10-class/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import User from './components/User'
+
+const App = () => {
+  const [allData, setAllData] = useState([])
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/users'
+      )
+      setAllData(response.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  return (
+    <div className="all-cards">
+      {allData.map((elem) => (
+        <User key={elem.id} elem={elem} />
+      ))}
+    </div>
+  )
+}
+
+export default App
